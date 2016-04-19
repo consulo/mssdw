cd7e6ff2354bfd4ca6d8d941f5661e905550abeb
VISTALL <vistall.valeriy@gmail.com>
2016-04-19 18:38:22 +0300

    reformat

--- CorApi/Metadata/MetadataPropertyInfo.cs
+++ CorApi/Metadata/MetadataPropertyInfo.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
 
-using Microsoft.Samples.Debugging.CorDebug;
 using Microsoft.Samples.Debugging.CorMetadata.NativeApi;
-using Microsoft.Samples.Debugging.CorDebug.NativeApi;
 using Microsoft.Samples.Debugging.Extensions;
 
 namespace Microsoft.Samples.Debugging.CorMetadata
 {
-	public class MetadataPropertyInfo: PropertyInfo
+	public class MetadataPropertyInfo : PropertyInfo
 	{
 		private IMetadataImport m_importer;
 		private int m_propertyToken;
@@ -27,9 +24,13 @@ namespace Microsoft.Samples.Debugging.CorMetadata
 		MetadataMethodInfo m_setter;
 		MetadataMethodInfo m_getter;
 
-		public CorMetadataImport CorMetadataImport { get; set; }
+		public CorMetadataImport CorMetadataImport
+		{
+			get;
+			set;
+		}
 
-		internal MetadataPropertyInfo (CorMetadataImport corMetadataImport, IMetadataImport importer, int propertyToken, MetadataType declaringType)
+		internal MetadataPropertyInfo(CorMetadataImport corMetadataImport, IMetadataImport importer, int propertyToken, MetadataType declaringType)
 		{
 			CorMetadataImport = corMetadataImport;
 			m_importer = importer;
@@ -47,160 +48,180 @@ namespace Microsoft.Samples.Debugging.CorMetadata
 			int rmdOtherMethod;
 			int pcOtherMethod;
 
-			m_importer.GetPropertyProps (
-				m_propertyToken,
-				out mdTypeDef,
-				null,
-				0,
-				out pchProperty,
-				out pdwPropFlags,
-				out ppvSig,
-				out pbSig,
-				out pdwCPlusTypeFlag,
-				out ppDefaultValue,
-				out pcchDefaultValue,
-				out m_pmdSetter,
-				out m_pmdGetter,
-				out rmdOtherMethod,
-				0,
-				out pcOtherMethod);
+			m_importer.GetPropertyProps(
+					m_propertyToken,
+					out mdTypeDef,
+					null,
+					0,
+					out pchProperty,
+					out pdwPropFlags,
+					out ppvSig,
+					out pbSig,
+					out pdwCPlusTypeFlag,
+					out ppDefaultValue,
+					out pcchDefaultValue,
+					out m_pmdSetter,
+					out m_pmdGetter,
+					out rmdOtherMethod,
+					0,
+					out pcOtherMethod);
 
-			StringBuilder szProperty = new StringBuilder (pchProperty);
-			m_importer.GetPropertyProps (
-				m_propertyToken,
-				out mdTypeDef,
-				szProperty,
-				pchProperty,
-				out pchProperty,
-				out pdwPropFlags,
-				out ppvSig,
-				out pbSig,
-				out pdwCPlusTypeFlag,
-				out ppDefaultValue,
-				out pcchDefaultValue,
-				out m_pmdSetter,
-				out m_pmdGetter,
-				out rmdOtherMethod,
-				0,
-				out pcOtherMethod);
+			StringBuilder szProperty = new StringBuilder(pchProperty);
+			m_importer.GetPropertyProps(
+					m_propertyToken,
+					out mdTypeDef,
+					szProperty,
+					pchProperty,
+					out pchProperty,
+					out pdwPropFlags,
+					out ppvSig,
+					out pbSig,
+					out pdwCPlusTypeFlag,
+					out ppDefaultValue,
+					out pcchDefaultValue,
+					out m_pmdSetter,
+					out m_pmdGetter,
+					out rmdOtherMethod,
+					0,
+					out pcOtherMethod);
 
 			m_propAttributes = (PropertyAttributes) pdwPropFlags;
-			m_name = szProperty.ToString ();
-			MetadataHelperFunctionsExtensions.GetCustomAttribute (importer, propertyToken, typeof (System.Diagnostics.DebuggerBrowsableAttribute));
+			m_name = szProperty.ToString();
+			MetadataHelperFunctionsExtensions.GetCustomAttribute(importer, propertyToken, typeof (System.Diagnostics.DebuggerBrowsableAttribute));
 
-			if (!m_importer.IsValidToken ((uint)m_pmdGetter))
+			if(!m_importer.IsValidToken((uint)m_pmdGetter))
 				m_pmdGetter = 0;
 
-			if (!m_importer.IsValidToken ((uint)m_pmdSetter))
+			if(!m_importer.IsValidToken((uint)m_pmdSetter))
 				m_pmdSetter = 0;
 		}
 
 		public override PropertyAttributes Attributes
 		{
-			get { return m_propAttributes; }
+			get
+			{
+				return m_propAttributes;
+			}
 		}
 
 		public override bool CanRead
 		{
-			get { return m_pmdGetter != 0; }
+			get
+			{
+				return m_pmdGetter != 0;
+			}
 		}
 
 		public override bool CanWrite
 		{
-			get { return m_pmdSetter != 0; }
+			get
+			{
+				return m_pmdSetter != 0;
+			}
 		}
 
-		public override MethodInfo[] GetAccessors (bool nonPublic)
+		public override MethodInfo[] GetAccessors(bool nonPublic)
 		{
-			throw new NotImplementedException ();
+			throw new NotImplementedException();
 		}
 
-		public override MethodInfo GetGetMethod (bool nonPublic)
+		public override MethodInfo GetGetMethod(bool nonPublic)
 		{
-			if (m_pmdGetter == 0)
+			if(m_pmdGetter == 0)
 				return null;
 
-			if (m_getter == null)
-				m_getter = new MetadataMethodInfo (CorMetadataImport, m_importer, m_pmdGetter);
+			if(m_getter == null)
+				m_getter = new MetadataMethodInfo(CorMetadataImport, m_importer, m_pmdGetter);
 
-			if (nonPublic || m_getter.IsPublic)
+			if(nonPublic || m_getter.IsPublic)
 				return m_getter;
 			return null;
 		}
 
-		public override ParameterInfo[] GetIndexParameters ( )
+		public override ParameterInfo[] GetIndexParameters()
 		{
-			MethodInfo mi = GetGetMethod ();
-			if (mi == null)
+			MethodInfo mi = GetGetMethod();
+			if(mi == null)
 				return new ParameterInfo[0];
-			return mi.GetParameters ();
+			return mi.GetParameters();
 		}
 
-		public override MethodInfo GetSetMethod (bool nonPublic)
+		public override MethodInfo GetSetMethod(bool nonPublic)
 		{
-			if (m_pmdSetter == 0)
+			if(m_pmdSetter == 0)
 				return null;
 
-			if (m_setter == null)
-				m_setter = new MetadataMethodInfo (CorMetadataImport, m_importer, m_pmdSetter);
+			if(m_setter == null)
+				m_setter = new MetadataMethodInfo(CorMetadataImport, m_importer, m_pmdSetter);
 
-			if (nonPublic || m_setter.IsPublic)
+			if(nonPublic || m_setter.IsPublic)
 				return m_setter;
 			return null;
 		}
 
-		public override object GetValue (object obj, BindingFlags invokeAttr, Binder binder, object[] index, System.Globalization.CultureInfo culture)
+		public override object GetValue(object obj, BindingFlags invokeAttr, Binder binder, object[] index, System.Globalization.CultureInfo culture)
 		{
-			throw new NotImplementedException ();
+			throw new NotImplementedException();
 		}
 
 		public override Type PropertyType
 		{
-			get { throw new NotImplementedException (); }
+			get
+			{
+				throw new NotImplementedException();
+			}
 		}
 
-		public override void SetValue (object obj, object value, BindingFlags invokeAttr, Binder binder, object[] index, System.Globalization.CultureInfo culture)
+		public override void SetValue(object obj, object value, BindingFlags invokeAttr, Binder binder, object[] index, System.Globalization.CultureInfo culture)
 		{
-			throw new NotImplementedException ();
+			throw new NotImplementedException();
 		}
 
 		public override Type DeclaringType
 		{
-			get {
+			get
+			{
 				return m_declaringType;
 			}
 		}
 
-		public override bool IsDefined (Type attributeType, bool inherit)
+		public override bool IsDefined(Type attributeType, bool inherit)
 		{
-			return GetCustomAttributes (attributeType, inherit).Length > 0;
+			return GetCustomAttributes(attributeType, inherit).Length > 0;
 		}
 
-		public override object[] GetCustomAttributes (Type attributeType, bool inherit)
+		public override object[] GetCustomAttributes(Type attributeType, bool inherit)
 		{
-			ArrayList list = new ArrayList ();
-			foreach (object ob in GetCustomAttributes (inherit)) {
-				if (attributeType.IsInstanceOfType (ob))
-					list.Add (ob);
+			ArrayList list = new ArrayList();
+			foreach (object ob in GetCustomAttributes(inherit))
+			{
+				if(attributeType.IsInstanceOfType(ob))
+					list.Add(ob);
 			}
-			return list.ToArray ();
+			return list.ToArray();
 		}
 
-		public override object[] GetCustomAttributes (bool inherit)
+		public override object[] GetCustomAttributes(bool inherit)
 		{
-			if (m_customAttributes == null)
-				m_customAttributes = MetadataHelperFunctionsExtensions.GetDebugAttributes (m_importer, m_propertyToken);
+			if(m_customAttributes == null)
+				m_customAttributes = MetadataHelperFunctionsExtensions.GetDebugAttributes(m_importer, m_propertyToken);
 			return m_customAttributes;
 		}
 
 		public override string Name
 		{
-			get { return m_name; }
+			get
+			{
+				return m_name;
+			}
 		}
 
 		public override Type ReflectedType
 		{
-			get { throw new NotImplementedException (); }
+			get
+			{
+				throw new NotImplementedException();
+			}
 		}
 	}
 }
